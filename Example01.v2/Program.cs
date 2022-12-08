6a3fc467c201b7d1689423132c3d789566743edb
Andrei Shvetsov <shved963@gmail.com>
2022-12-08 21:04:39 +0300

    first commit

---
+++ Example01.v2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая принимает на
+// вход три числа и проверяет, может ли существовать
+// треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника:
+// каждая сторона треугольника меньше суммы двух других сторон.
+Console.Clear();
+
+Console.WriteLine("Введите 3 числа");
+
+int AB = IntoInt();
+int AC = IntoInt();
+int BC = IntoInt();
+
+if (AB == 0 && AC == 0 && BC == 0)
+{
+    Console.WriteLine("Incorrect number");
+    return;
+}
+
+bool result = VerificationOfTriangleInequalityTheorem(AB, AC, BC);
+
+Console.WriteLine(result);
+
+bool VerificationOfTriangleInequalityTheorem(int ab, int ac, int bc)
+{
+    if (ab < (ac + bc) && ac < (ab + bc) && bc< (ac + ab))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int IntoInt()
+{
+    bool isParsed = int.TryParse(Console.ReadLine(), out int num);
+    if (!isParsed || num < 1)
+    {
+        return 0;
+    }
+    else
+    {
+        return num;
+    }
+}
---
+++ Example01/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+
+int[] array = CreateRandomArray();
+
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+        Console.Write(" ");
+    }
+}
+
+
+
+
+int[] CreateRandomArray()
+{
+    Random random = new Random();
+
+    int[] array = new int[10];
+
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(int.MinValue,int.MaxValue);
+
+    }
+    
+    return array;
+}
